14c3ac2da9c9b76ffe89a9a23d44e870f1d6a11e
hy.jang <hy.jang@autowini.com>
2021-08-23 16:16:04 +0900

    Typescript로 컴포넌트 작성하기

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
@@ -5,4 +7,14 @@ module.exports = {
     "@storybook/addon-essentials",
     "@storybook/addon-knobs",
   ],
+  typescript: {
+    check: false,
+    checkOptions: {},
+    reactDocgen: "react-docgen-typescript",
+    reactDocgenTypescriptOptions: {
+      shouldExtractLiteralValuesFromEnum: true,
+      propFilter: (prop) =>
+        prop.parent ? !/node_modules/.test(prop.parent.fileName) : true,
+    },
+  },
 };
